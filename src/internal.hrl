946c190012181c97abe7f71a34462c77b33d3dff
Eric Merritt <ericbmerritt@gmail.com>
2012-06-15 16:10:51 -0500

    minor reformatting to internal.hrl (make emacs happy)

--- src/internal.hrl
+++ src/internal.hrl
@@ -1,5 +1,7 @@
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92 -*-
+%% ex: ts=4 sw=4 et
 -record(config, {
-    s3_url="http://s3.amazonaws.com"::string(),
-    access_key_id::string(),
-    secret_access_key::string()
+          s3_url="http://s3.amazonaws.com"::string(),
+          access_key_id::string(),
+          secret_access_key::string(),
 }).
