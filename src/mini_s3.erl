92415ed53cab1641574f66397492aa9903032210
Seth Falcon <seth@opscode.com>
2012-09-05 12:20:51 -0700

    Dialyzer warning fixes

--- src/mini_s3.erl
+++ src/mini_s3.erl
@@ -64,6 +64,11 @@
                                | request_payment
                                | versioning.
 
+-type settable_bucket_attribute_name() :: acl
+                                        | logging
+                                        | request_payment
+                                        | versioning.
+
 -type bucket_acl() :: private
                     | public_read
                     | public_read_write
@@ -348,7 +353,7 @@ decode_permission("READ_ACP")     -> read_acp.
 
 %% @doc Canonicalizes a proplist of {"Header", "Value"} pairs by
 %% lower-casing all the Headers.
--spec canonicalize_headers([{Header::string(), Value::string()}]) ->
+-spec canonicalize_headers([{string() | binary() | atom(), Value::string()}]) ->
                                   [{LowerCaseHeader::string(), Value::string()}].
 canonicalize_headers(Headers) ->
     [{string:to_lower(to_string(H)), V} || {H, V} <- Headers ].
@@ -620,12 +625,21 @@ extract_bucket(Node) ->
                     {creation_date, "CreationDate", time}],
                    Node).
 
--spec put_object(string(), string(), iolist(), proplists:proplist(), [{string(), string()}] | config()) -> proplists:proplist().
+-spec put_object(string(),
+                 string(),
+                 iolist(),
+                 proplists:proplist(),
+                 [{string(), string()}]) -> [{'version_id', _}, ...].
 
 put_object(BucketName, Key, Value, Options, HTTPHeaders) ->
     put_object(BucketName, Key, Value, Options, HTTPHeaders, default_config()).
 
--spec put_object(string(), string(), iolist(), proplists:proplist(), [{string(), string()}], config()) -> proplists:proplist().
+-spec put_object(string(),
+                 string(),
+                 iolist(),
+                 proplists:proplist(),
+                 [{string(), string()}],
+                 config()) -> [{'version_id', _}, ...].
 
 put_object(BucketName, Key, Value, Options, HTTPHeaders, Config)
   when is_list(BucketName), is_list(Key), is_list(Value) orelse is_binary(Value),
@@ -657,12 +671,15 @@ set_object_acl(BucketName, Key, ACL, Config)
     XMLText = list_to_binary(xmerl:export_simple([XML], xmerl_xml)),
     s3_simple_request(Config, put, BucketName, [$/|Key], "acl", [], XMLText, []).
 
--spec set_bucket_attribute(string(), atom(), term()) -> ok.
+-spec set_bucket_attribute(string(),
+                           settable_bucket_attribute_name(),
+                           'bucket_owner' | 'requester' | [any()]) -> ok.
 
 set_bucket_attribute(BucketName, AttributeName, Value) ->
     set_bucket_attribute(BucketName, AttributeName, Value, default_config()).
 
--spec set_bucket_attribute(string(), atom(), term(), config()) -> ok.
+-spec set_bucket_attribute(string(), settable_bucket_attribute_name(),
+                           'bucket_owner' | 'requester' | [any()], config()) -> ok.
 
 set_bucket_attribute(BucketName, AttributeName, Value, Config)
   when is_list(BucketName) ->
