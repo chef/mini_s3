233bb032da948f60492483477a2f5b5f02ec68e3
Lincoln Baker <lbaker@chef.io>
2020-08-20 12:11:57 -0500

    remove unused/broken tests; fix other tests
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- test/mini_s3_tests.erl
+++ test/mini_s3_tests.erl
@@ -20,122 +20,43 @@
 
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
-%-include("../src/erlcloud_aws.hrl").
 -include("../src/internal.hrl").
+
+-spec format_s3_uri(aws_config(), string()) -> string().
+format_s3_uri(Config, Bucket) ->
+    erlcloud_s3:get_object_url(Bucket, "", Config).
 
 format_s3_uri_test_() ->
     Config = fun(Url, Type) ->
-                     %#config{s3_url = Url, bucket_access_type = Type}
-                     %#aws_config{s3_url = Url, s3_bucket_access_method = Type}
+                     %#config{s3_url = Url, s3_bucket_access_method = Type}
+                     %#aws_config{s3_url = Url, s3_ = Type}
                      mini_s3:new("", "", Url, Type)
              end,
     Tests = [
              %% hostname
-             {"https://my-aws.me.com", vhost, "https://bucket.my-aws.me.com:443"},
-             {"https://my-aws.me.com", path,  "https://my-aws.me.com:443/bucket"},
+             {"https://my-aws.me.com", vhost, "https://bucket.my-aws.me.com:443/"},
+             {"https://my-aws.me.com", path,  "https://my-aws.me.com:443/bucket/"},
 
              %% ipv4
-             {"https://192.168.12.13", path,  "https://192.168.12.13:443/bucket"},
+             {"https://192.168.12.13", path,  "https://192.168.12.13:443/bucket/"},
 
              %% ipv6
              {"https://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]", path,
-              "https://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:443/bucket"},
+              "https://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:443/bucket/"},
 
              %% These tests document current behavior. Using
              %% vhost with an IP address does not make sense,
              %% but leaving as-is for now to avoid adding the
              %% is_it_an_ip_or_a_name code.
-             {"https://192.168.12.13", vhost, "https://bucket.192.168.12.13:443"},
+             {"https://192.168.12.13", vhost, "https://bucket.192.168.12.13:443/"},
 
              {"https://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]", vhost,
-              "https://bucket.[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:443"}
+              "https://bucket.[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:443/"}
             ],
-    [ ?_assertEqual(Expect, mini_s3:format_s3_uri(Config(Url, Type), "bucket"))
+    [ ?_assertEqual(Expect, format_s3_uri(Config(Url, Type), "bucket"))
       || {Url, Type, Expect} <- Tests ].
 
-%% 2015-01-27 00:00:00 = 1422316800 = ?MIDNIGHT
--define(MIDNIGHT, 1422316800).
--define(DAY, 86400).
--define(HOUR, 3600).
-
-%expiration_time_test_() ->
-%    Tests = [
-%             %% {TTLSecs, IntervalSecs, MockedTimestamp, ExpectedExpiry}
-%             {{3600, 900}, {{2015,1,27},{0,0,0}}  , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,0,10}} , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,1,0}}  , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,1,10}} , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,3,0}}  , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,3,30}} , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,5,0}}  , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,10,0}} , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,14,0}} , (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,14,59}}, (?MIDNIGHT + ?HOUR + 900)},
-%             {{3600, 900}, {{2015,1,27},{0,15,0}} , (?MIDNIGHT + ?HOUR + 1800)},
-%             {{3600, 900}, {{2015,1,27},{0,15,1}} , (?MIDNIGHT + ?HOUR + 1800)},
-%             {{3600, 900}, {{2015,1,27},{0,29,59}}, (?MIDNIGHT + ?HOUR + 1800)},
-%             {{3600, 900}, {{2015,1,27},{0,30,0}} , (?MIDNIGHT + ?HOUR + 2700)},
-%             {{3600, 900}, {{2015,1,27},{0,44,59}}, (?MIDNIGHT + ?HOUR + 2700)},
-%             {{3600, 900}, {{2015,1,27},{0,45,0}} , (?MIDNIGHT + ?HOUR + 3600)},
-%             {{3600, 900}, {{2015,1,27},{0,59,59}}, (?MIDNIGHT + ?HOUR + 3600)},
-%             {{3600, 900}, {{2015,1,27},{1,0,0}}  , (?MIDNIGHT + ?HOUR + 4500)},
-%
-%             %% There are 86400 seconds in a day. What happens if the interval is not evenly
-%             %% divisible in that time? Take 7m for example. 420 secs goes into a day 205.71
-%             %% times which is a remainder of 300 seconds. We should make sure that we
-%             %% restart the intervals at midnight, so we don't have day to day drift
-%
-%             {{3600, 420}, {{2015,1,27},{23,59,0}} , (?MIDNIGHT + ?DAY + ?HOUR)},
-%             {{3600, 420}, {{2015,1,28},{0,0,0}}   , (?MIDNIGHT + ?DAY + ?HOUR + 420)},
-%
-%             %% Let's test the old functionality too
-%             {3600, {{2015,1,27},{0,0,0}} , (?MIDNIGHT + ?HOUR)},
-%             {3600, {{2015,1,27},{0,0,1}} , (?MIDNIGHT + ?HOUR + 1)},
-%             {3600, {{2015,1,27},{0,1,1}} , (?MIDNIGHT + ?HOUR + 61)},
-%             {3600, {{2015,1,28},{0,1,1}} , (?MIDNIGHT + ?DAY + ?HOUR + 61)}
-%            ],
-%
-%    TestFun = fun(Arg, MockedTime) ->
-%                      meck:new(mini_s3, [unstick, passthrough]),
-%                      meck:expect(mini_s3, universaltime, fun() -> MockedTime end),
-%                      Expiry = mini_s3:expiration_time(Arg),
-%                      meck:unload(mini_s3),
-%                      Expiry
-%              end,
-%    [ ?_assertEqual(Expect, TestFun(Arg, MockedTimestamp))
-%      || {Arg, MockedTimestamp, Expect} <- Tests].
-
-%s3_uri_test_() ->
-%%    Config = #config{
-%%               access_key_id = "access_key_id",
-%%               secret_access_key = "secret_access_key"
-%%               },
-%
-%    Config = mini_s3:new("access_key_id", "secret_access_key", "http://host.com"),
-%
-%    RawHeaders = [],
-%
-%    TestFun = fun({Method, BucketName, Key, Lifetime, MockedTime}) ->
-%                      meck:new(mini_s3, [no_link, passthrough]),
-%                      meck:expect(mini_s3, universaltime, fun() -> MockedTime end),
-%%                      meck:expect(mini_s3, make_signed_url_authorization, fun(_,_,_,_,_) -> {"", <<"k6E/2haoGH5vGU9qDTBRs1qNGKA=">>} end),
-%
-%                      URL = binary_to_list(mini_s3:s3_url(Method, BucketName, Key, Lifetime, RawHeaders, Config)),
-%                      io:format("URL: ~p~n", [URL]),
-%                      io:format("History: ~p~n", [meck:history(mini_s3)]),
-%                      meck:unload(mini_s3),
-%
-%                      URL
-%              end,
-%
-%    Tests = [
-%             {{'GET', "BUCKET", "KEY", 3600, {{2015,1,27},{0,0,0}}}, "http://s3.amazonaws.com:80/BUCKET/KEY?AWSAccessKeyId=access_key_id&Expires=1422320400&Signature=k6E/2haoGH5vGU9qDTBRs1qNGKA%3D"},
-%             {{'GET', "BUCKET2", "KEY2", {3600, 900}, {{2015,1,27},{0,0,0}}}, "http://s3.amazonaws.com:80/BUCKET2/KEY2?AWSAccessKeyId=access_key_id&Expires=1422321300&Signature=k6E/2haoGH5vGU9qDTBRs1qNGKA%3D"}
-%            ],
-%
-%    [ ?_assertEqual(Expect, TestFun(Args)) || {Args, Expect} <- Tests].
-
-% this should be named make_expire_win_test(), but timeout doesn't work unless main_test_()
+% this should be named make_expire_win_test(), but timeout doesn't work unless it's named main_test_()
 main_test_() ->
     {timeout, 60,
         fun() ->
@@ -250,18 +171,3 @@ get_host_toggleport_test() ->
     "http://host" = mini_s3:get_host_toggleport("http://host:123", Config3),
     Config4 = mini_s3:new("", "", "https://host:123"),
     "https://host" = mini_s3:get_host_toggleport("https://host:123", Config4).
-
-% construct url from config
-get_url_test() ->
-    Config0 = mini_s3:new("", "", "http://1.2.3.4"),
-    "http://1.2.3.4"      = mini_s3:get_url_noport(Config0),
-    "http://1.2.3.4:80"   = mini_s3:get_url_port(  Config0),
-    Config1 = mini_s3:new("", "", "https://1.2.3.4"),
-    "https://1.2.3.4"     = mini_s3:get_url_noport(Config1),
-    "https://1.2.3.4:443" = mini_s3:get_url_port(  Config1),
-    Config2 = mini_s3:new("", "", "http://1.2.3.4:443"),
-    "http://1.2.3.4"      = mini_s3:get_url_noport(Config2),
-    "http://1.2.3.4:443"  = mini_s3:get_url_port(  Config2),
-    Config3 = mini_s3:new("", "", "https://1.2.3.4:80"),
-    "https://1.2.3.4"     = mini_s3:get_url_noport(Config3),
-    "https://1.2.3.4:80"  = mini_s3:get_url_port(  Config3).
